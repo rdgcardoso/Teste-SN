d2fa8edd423ed47270dc5b356e6c5a8247ad37b6
RODRIGO-PC\rdgca <rdgcardoso1@hotmail.com>
2017-09-07 03:02:47 -0300

    ajustes no ranking

--- app/src/main/java/br/com/teste/testerecyclerview/app/controller/BaladaRankingFragment.java
+++ app/src/main/java/br/com/teste/testerecyclerview/app/controller/BaladaRankingFragment.java
@@ -1,21 +1,48 @@
 package br.com.teste.testerecyclerview.app.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.teste.testerecyclerview.R;
+import br.com.teste.testerecyclerview.app.adapter.BaladaAdapter;
+import br.com.teste.testerecyclerview.app.adapter.CustomItemClickListener;
+import br.com.teste.testerecyclerview.app.dto.BaladaDTO;
+import br.com.teste.testerecyclerview.app.dto.UsuarioDTO;
+import br.com.teste.testerecyclerview.app.resources.CodigoRetornoHTTP;
 import br.com.teste.testerecyclerview.app.task.ConsultarRankingBaladasTask;
+import br.com.teste.testerecyclerview.app.util.RetrofitHelper;
 import br.com.teste.testerecyclerview.app.util.SharedPreferencesHelper;
+import br.com.teste.testerecyclerview.app.ws.BaladaEndpoint;
+import br.com.teste.testerecyclerview.app.ws.RankingBaladasEndpoint;
+import br.com.teste.testerecyclerview.app.ws.UsuarioEndpoint;
+import br.com.teste.testerecyclerview.domain.model.Balada;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class BaladaRankingFragment extends Fragment {
 
     private SharedPreferencesHelper sharedPreferencesHelper;
+    private List<BaladaDTO> baladaDTOList;
+    private Balada balada;
+    private List<Balada> baladaList;
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Nullable
     @Override
@@ -30,20 +57,100 @@ public class BaladaRankingFragment extends Fragment {
         Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
         toolbar.setTitle("StartNight");
 
-        SwipeRefreshLayout swipeRefreshLayout = getActivity().findViewById(R.id.swipeRefreshLayout);
         sharedPreferencesHelper = new SharedPreferencesHelper(getContext());
 
+        swipeRefreshLayout = getActivity().findViewById(R.id.swipeRefreshLayout);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                new ConsultarRankingBaladasTask(getContext(), getView(), sharedPreferencesHelper.recuperarToken()).execute();
+                carregarBaladaRanking();
             }
         });
 
         View progressBar = getActivity().findViewById(R.id.progressIndeterminateBar);
         progressBar.setVisibility(View.VISIBLE);
 
-        new ConsultarRankingBaladasTask(getContext(), sharedPreferencesHelper.recuperarToken()).execute();
+        recyclerView = (RecyclerView) getActivity().findViewById(R.id.recycler);
+        RecyclerView.LayoutManager layout = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layout);
 
+        carregarBaladaRanking();
+    }
+
+    private void carregarBaladaRanking() {
+
+        RankingBaladasEndpoint endpoint = RetrofitHelper.with(getContext()).createRankingBaladasEndpoint();
+
+        baladaList = new ArrayList<>();
+
+        Call<List<BaladaDTO>> call = endpoint.consultarRankingBaladas(sharedPreferencesHelper.recuperarToken());
+
+        call.enqueue(new Callback<List<BaladaDTO>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<BaladaDTO>> call, @NonNull Response<List<BaladaDTO>> response) {
+
+                baladaDTOList = response.body();
+                if (response.isSuccessful()) {
+                    if (baladaDTOList != null) {
+                        Log.d("LRDG", "baladaDTOList preenchido: " + baladaDTOList.toString());
+
+                        for (BaladaDTO baladaDTO : baladaDTOList) {
+                            if (baladaDTO.isAtivo()) {
+                                balada = new Balada(
+                                        baladaDTO.getId(),
+                                        baladaDTO.getTipo_musicas(),
+                                        baladaDTO.getNome(),
+                                        baladaDTO.getDescricao(),
+                                        baladaDTO.getCep(),
+                                        baladaDTO.getEstado(),
+                                        baladaDTO.getCidade(),
+                                        baladaDTO.getBairro(),
+                                        baladaDTO.getLogradouro(),
+                                        baladaDTO.getNumero(),
+                                        baladaDTO.getComplemento(),
+                                        baladaDTO.getAvaliacao(),
+                                        baladaDTO.getSite(),
+                                        baladaDTO.getPreco_medio(),
+                                        baladaDTO.isAtivo(),
+                                        baladaDTO.getFoto()
+                                );
+                                baladaList.add(balada);
+                            }
+                        }
+
+                        BaladaAdapter baladaAdapter = new BaladaAdapter(getContext(), (ArrayList<Balada>) baladaList, new CustomItemClickListener() {
+                            @Override
+                            public void onItemClick(View v, int position) {
+                                Log.d("LRDG", "Clicado position: " + position);
+
+                                Balada balada;
+                                balada = baladaList.get(position);
+
+                                Intent i = new Intent(getContext(), BaladaDetalhesActivity.class);
+                                i.putExtra("balada", balada);
+                                startActivity(i);
+                            }
+                        });
+                        recyclerView.setAdapter(baladaAdapter);
+
+                        if (swipeRefreshLayout.isRefreshing()) {
+                            swipeRefreshLayout.setRefreshing(false);
+                            Toast.makeText(getContext(), "Ranking atualizado...", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                } else {
+                    Log.d("LRDG", "Erro!");
+                    CodigoRetornoHTTP codigo = new CodigoRetornoHTTP(response.code());
+                    codigo.notAuthorized(getContext());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<BaladaDTO>> call, @NonNull Throwable t) {
+                Log.d("LRDG", "Falha!");
+            }
+
+        });
     }
 }
